6bc9b34d0a7e58743d3599b8cc025896b9e430ce
Matt Connors <foxitb123@gmail.com>
2025-05-22 19:34:20 +0300

    Implement Caesar cipher logic
    - Completed Encrypt() and Decrypt() methods in CaesarCipher class
    - Added default values (alphabet, plaintext, mode) in Common.Defaults for reuse across the library

--- CipherPlayground.Library/CaesarCipher.cs
+++ CipherPlayground.Library/CaesarCipher.cs
@@ -1,14 +1,46 @@
+using static CipherPlayground.Library.Common;
+
 namespace CipherPlayground.Library
 {
     public class CaesarCipher
     {
-        public static string Encrypt(string plaintext, int key, CipherMode mode = CipherMode.Strict)
+        public static string Encrypt(string plaintext, int key, CipherMode mode = Defaults.DefaultMode)
         {
-            char[] alphabet = ['A', 'B', 'C'];
+            char[] alphabet = Defaults.DefaultAlphabet;
+            int alphabetLength = alphabet.Length;
+            plaintext = plaintext.ToUpper();
+
+            string result = string.Empty;
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                char currentChar = plaintext[i];
+                if (alphabet.Contains(currentChar))
+                {
+                    int currentCharIndex = Array.IndexOf(alphabet, currentChar);
+                    int newIndex = (currentCharIndex + key) % alphabetLength;
+                    if (newIndex < 0) { newIndex += alphabetLength; }
+                    result += alphabet[newIndex];
+                } else
+                {
+                    switch (mode)
+                    {
+                        case CipherMode.Strict:
+                            throw new Exception($"The plaintext must not contain invalid characters. '{currentChar}' is not valid");
+                        case CipherMode.Loose:
+                            continue;
+                        case CipherMode.Preserve:
+                            result += currentChar;
+                            break;
+                    }
+                }
+            }
+
+            return result;
         }
-        public static string Decrypt(string plaintext, int key)
+        public static string Decrypt(string ciphertext, int key, CipherMode mode = Defaults.DefaultMode)
         {
-            return Encrypt(plaintext, key)
+            return Encrypt(ciphertext, (key * -1), mode);
         }
     }
 }
--- CipherPlayground.Library/Common.cs
+++ CipherPlayground.Library/Common.cs
@@ -2,6 +2,12 @@ namespace CipherPlayground.Library
 {
     public class Common
     {
+        public static class Defaults
+        {
+            public static readonly char[] DefaultAlphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+            public const string DefaultPlaintext = "HELLO";
+            public const CipherMode DefaultMode = CipherMode.Preserve;
+        }
         public enum CipherMode
         {
             Strict,
